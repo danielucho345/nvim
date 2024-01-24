115fe1c59b4677df550069a5f4861d2be97e8cba
danielucho345 <87717966+danielucho123@users.noreply.github.com>
2024-01-24 17:11:26 -0600

    update

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,46 +1,3 @@
--- local M = {
--- }
--- function M.config()
---   local configs = require "nvim-treesitter.configs"
---
---   configs.setup {
---     highlight = {
---       additional_vim_regex_highlighting = true,
---       enable = true,
---     },
---     ensure_installed = {
---       "c",
---       "lua",
---       "vim",
---       "vimdoc",
---       "query",
---       "python",
---       "rust",
---       "typescript",
---       "javascript",
---       "go",
---       "html",
---       "htmldjango",
---       "css",
---     },                       -- put the language you want in this array
---     -- ensure_installed = "all", -- one of "all" or a list of languages
---     ignore_install = { "" }, -- List of parsers to ignore installing
---     sync_install = true,     -- install languages synchronously (only applied to `ensure_installed`)
---     autopairs = {
---       enable = true,
---     },
---     indent = { enable = true, disable = {} },
---
---     context_commentstring = {
---       enable = true,
---       enable_autocmd = false,
---     },
---     additional_vim_regex_highlighting = true,
---   }
--- end
---
--- return M
-
 local configs = require "nvim-treesitter.configs"
 
 configs.setup {
@@ -62,7 +19,6 @@ configs.setup {
     "css",
   },                       -- put the language you want in this array
   -- ensure_installed = "all", -- one of "all" or a list of languages
-  ignore_install = { "" }, -- List of parsers to ignore installing
   sync_install = false,     -- install languages synchronously (only applied to `ensure_installed`)
   autopairs = {
     enable = true,
