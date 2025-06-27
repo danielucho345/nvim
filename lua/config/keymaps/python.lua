76b27fad48f45454f026c39ce5eb7fea424c9bde
Daniel Mora <danielmora@MacBook-Air-de-Daniel.local>
2025-06-26 22:48:19 -0600

    add python

--- lua/config/keymaps/python.lua
+++ lua/config/keymaps/python.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>cM", function()
-  local relative_path = vim.fn.fnamemodify(vim.fn.expand("%"), ":.")
+  local relative_path = vim.fn.fnamemodify(vim.fn.expand("%:r"), ":.")
   local module_path = relative_path:gsub("/", "."):gsub("%.lua$", "")
   vim.fn.setreg("+", "python -m " .. module_path) -- Copy to clipboard
   vim.notify("Copied to clipboard: python -m " .. module_path)
